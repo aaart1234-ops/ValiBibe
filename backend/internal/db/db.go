e328006cdbd8f7120c43f753413557116070c386
Андрей Артамонов <andrejartamonov@MacBook-Pro-Andrej.local>
2025-02-27 17:28:31 +0300

    Автоматизация запуска. Добавил Makefile, docker-compose.yml, Dockerfile

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/jackc/pgx/v5/pgxpool" // Импорт библиотеки для работы с пулом соединений PostgreSQL
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // DB — глобальная переменная для хранения пула соединений с БД.
@@ -13,33 +14,29 @@ var DB *pgxpool.Pool
 
 // ConnectDB устанавливает соединение с базой данных.
 func ConnectDB() {
-	// Строка подключения (DSN — Data Source Name), содержит:
-	// - логин (postgres)
-	// - пароль (password)
-	// - адрес хоста (localhost)
-	// - порт (5432)
-	// - имя базы данных (spaced_repetition_db)
-	// - sslmode=disable (отключает SSL, используется для локальной разработки)
-	dsn := "postgres://postgres:password@localhost:5432/spaced_repetition_db?sslmode=disable"
+	// Берем URL базы из переменной окружения
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("❌ DATABASE_URL не задан!")
+	}
 
-	// Парсим строку подключения в структуру конфигурации pgxpool
+	// Парсим строку подключения
 	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
-		log.Fatalf("Ошибка парсинга DSN: %v", err) // Завершаем выполнение программы, если произошла ошибка
+		log.Fatalf("❌ Ошибка парсинга DSN: %v", err)
 	}
 
-	// Создаём пул соединений с БД на основе конфигурации
+	// Создаём пул соединений с БД
 	dbpool, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
-		log.Fatalf("Ошибка подключения к БД: %v", err) // Если не удалось подключиться — завершаем программу
+		log.Fatalf("❌ Ошибка подключения к БД: %v", err)
 	}
 
-	// Проверяем соединение с БД с помощью Ping
+	// Проверяем соединение
 	if err := dbpool.Ping(context.Background()); err != nil {
-		log.Fatalf("Ошибка проверки соединения: %v", err)
+		log.Fatalf("❌ Ошибка проверки соединения: %v", err)
 	}
 
-	// Если все прошло успешно, выводим сообщение и сохраняем пул соединений в глобальную переменную DB
 	fmt.Println("✅ Подключение к БД успешно!")
 	DB = dbpool
 }
